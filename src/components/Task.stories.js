77cf042e662c67dc01a542fdbc7ce211111e7d5a
Emanuel <manupalacios19@gmail.com>
2021-07-15 14:34:02 -0300

    Add Task component

---
+++ src/components/Task.stories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Task from './Task';
+
+export default {
+    component: Task,
+    title: 'Task',
+};
+
+const Template = args => <Task {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    task: {
+        id: 1,
+        title: 'Test Task',
+        state: 'TASK_INBOX',
+        updatedAt: new Date(2021, 0, 1, 9, 0),
+    }
+}
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+    ...Default.args,
+    state: 'TASK_PINNED',
+}
+
+export const Archived = Template.bind({});
+Archived.args = {
+    ...Default.args,
+    state: 'TASK_ARCHIVED',
+}
